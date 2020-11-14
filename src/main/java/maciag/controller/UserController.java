6767ac9f363d307e3f7af9c874e89246875ff230
Maciag312 <dariakingasikorska@gmail.com>
2020-11-14 14:04:56 +0100

    cross origin annotation

--- src/main/java/maciag/controller/UserController.java
+++ src/main/java/maciag/controller/UserController.java
@@ -5,14 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -28,6 +21,7 @@ import maciag.service.UserService;
 @RestController
 @RequestMapping("/users")
 @Api(tags = "users")
+@CrossOrigin
 public class UserController {
 
   @Autowired
@@ -41,6 +35,7 @@ public class UserController {
   @ApiResponses(value = {//
       @ApiResponse(code = 400, message = "Something went wrong"), //
       @ApiResponse(code = 422, message = "Invalid username/password supplied")})
+  @CrossOrigin(origins = "*")
   public String login(//
       @ApiParam("Username") @RequestParam String username, //
       @ApiParam("Password") @RequestParam String password) {
